e0645744f915849134bc1fa1af048ce18ba1790a
zenna <97446606+zenna9@users.noreply.github.com>
2024-12-22 23:10:35 +0900

    1-2) createVNode

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,14 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    // 기존
+    // children: (children.flat(Infinity)).filter((childX) => !!childX)
+    // 개선
+    //개선1 : children: (children.flat(Infinity)).filter(Boolean) -> 이 경우 0을 falsy로 인식하여 리턴 제외함
+    //개선2 : 최종
+    children: children
+      .flat(Infinity)
+      .filter((childX) => (childX === 0 ? true : Boolean(childX))), // 완전 평탄화
+  };
 }
