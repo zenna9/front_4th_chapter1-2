446da6c78afcc5c83339fdbf151ace6da651f90e
zenna <97446606+zenna9@users.noreply.github.com>
2024-12-27 02:49:45 +0900

    Feat:1-2) createElement

--- src/lib/createElement.js
+++ src/lib/createElement.js
@@ -1,5 +1,41 @@
-import { addEvent } from "./eventManager";
+export function createElement(vNode) {
+    if (vNode === null || vNode === undefined || typeof vNode === "boolean") {
+        return document.createTextNode(""); 
+    }
+    
+    if (typeof vNode === "string" || typeof vNode === "number") {
+        return document.createTextNode(vNode);
+    }
 
-export function createElement(vNode) {}
+    if(Array.isArray(vNode)){
+        const fragment = document.createDocumentFragment();
+        vNode.forEach((node) =>{
+            const doms = createNodeWithChildren(node);
+            fragment.appendChild(doms);
+        })
+        return fragment;
+    }else{ // typeof "Object"
+        return createNodeWithChildren(vNode);
+    }
+}
 
-function updateAttributes($el, props) {}
+function updateAttributes($el, props) {
+
+}
+
+function createNodeWithChildren(node){
+    const domX = document.createElement(node.type);
+    //props 삽입
+    for(let propK in node.props){
+        if (propK === 'className') {
+            domX.setAttribute('class', node.props[propK]);
+        } else {
+            domX.setAttribute(propK, node.props[propK]);
+        }
+    }
+    //child 삽입
+    node.children.forEach((child) =>{
+        domX.appendChild(createElement(child));
+    })
+    return domX;
+}
